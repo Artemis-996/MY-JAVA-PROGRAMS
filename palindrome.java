f945d8e827ef09bb7af770ebc3e9b9e4e6ecc8af
Ankit Gajbhiye <106885420+Artemis-996@users.noreply.github.com>
2023-03-13 19:07:13 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -1,36 +1,26 @@
-package programs;
-
 import java.util.Scanner;
+class  Palindrome
+{
+	public static void main(String[] args) 
+	{
+		Scanner s = new Scanner(System.in);
 
-public class palindrome {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter Number");
-		int num = sc.nextInt();
-		
-		int temp = num;
+		System.out.println("Enter Number : ");
+		int num = s.nextInt();
 		int rev = 0;
-		int lst;
-		
-		while (num>0)
+
+		for (int a = num; a>0 ; a/=10 )
 		{
-			lst = num % 10;
-			rev = rev * 10 + lst;
-			num = Math.floorDiv(num, 10);
+			int rem = a%10;
+			rev = (rev*10)+rem;
 		}
-		if (rev==temp)
+		if (rev==num)
 		{
-			System.out.println(temp + " is a palindrome number");
+			System.out.println("Enter number is palindrome");
 		}
 		else
 		{
-			System.out.println(temp + " is not palindrome number");
+			System.out.println("Entered Number is not Palindrome");
 		}
-		
-		
-	
 	}
-
 }
