5c44fa8f7fb8c58ee37f2096cb7f700d6baa24af
Ankit Gajbhiye <106885420+Artemis-996@users.noreply.github.com>
2023-03-14 17:41:45 +0530

    Program to calculate factorial
    1. first take input
    2. store in scaner.
    3.As factorial are multiplied hence create a Fact Variable store it with default value 1.
    4.now run a loop till number become >0

---
+++ Factorial.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Factorial 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter Number : ");
+		int num = sc.nextInt();
+
+		int fact = 1;
+
+		while (num>0)
+		{
+			fact = fact*num;
+			num--;
+		}
+		System.out.println(fact);
+	}
+}
