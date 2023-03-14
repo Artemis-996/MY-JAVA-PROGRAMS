59a80307f2b4d41668bd72a3b06139669ac2e0a8
Ankit Gajbhiye <106885420+Artemis-996@users.noreply.github.com>
2023-03-14 23:07:05 +0530

    Count of Even and Odd digits in number.
    ex:- 1234
    odd = 1 & 3 = 2
    even = 2 & 4 = 2

--- EvenOddCount.java
+++ EvenOddCount.java
@@ -3,7 +3,7 @@ class EvenOddCount
 {
 	public static void main(String[] args) 
 	{
-		Scanner s = new Scanner(System.in);
+		Scanner s = new Scanner (System.in);
 		
 		System.out.println("Enter Number : ");
 		int num = s.nextInt();
