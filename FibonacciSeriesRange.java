741a82af2164ff50651beb1d80903736fd2d6394
Ankit Gajbhiye <106885420+Artemis-996@users.noreply.github.com>
2023-03-26 22:06:13 +0530

    FIBONACCI SERIES

---
+++ FibonacciSeriesRange.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+class FibonacciSeriesRange 
+{
+	public static void main(String[] args) 
+	{
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter Starting Number ");
+		int start = s.nextInt();
+
+		System.out.println("Enter Ending Number ");
+		int end = s.nextInt();
+		int n1 = 0;
+		int n2 = 1;
+		int n3;
+
+		while (start<=end)
+		{
+			while (true)
+			{
+				n3 = n1+n2;
+				if (n1==start)
+				{
+					System.out.println(n1);
+					break;
+				}
+				else if (n1>start)
+				{
+					break;
+				}
+				n1=n2;
+				n2=n3;
+			}
+			start++;
+		}
+
+	}
+}
